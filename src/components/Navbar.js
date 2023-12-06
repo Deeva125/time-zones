951813380ef52844a230a3d7c9ff884ee90b99ca
Deeva Singhal <deevasinghal75071@gmail.com>
2023-12-05 18:53:36 -0600

    changed home route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,11 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/time-zones"
+                >
                   Home
                 </Link>
               </li>
